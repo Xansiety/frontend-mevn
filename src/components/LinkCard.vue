03e768d6b5ed46e52d718c2ac59bf8e2a79b35dc
xansiety <ferando543@outlook.com>
2022-10-16 14:18:51 -0500

    Se crea componente LinkCard

---
+++ src/components/LinkCard.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from "vue";
+</script>
+
+<template>
+  <q-card class="my-card">
+    <q-card-section>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit
+    </q-card-section>
+  </q-card>
+</template>
+
+<style lang="scss" scoped></style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,12 +1,17 @@
 <script setup>
 import { useUrlStore } from "../stores/url-store";
 import AddLink from "src/components/AddLink.vue";
+import LinkCard from "src/components/LinkCard.vue";
 const urlStore = useUrlStore();
 </script>
 
 <template>
   <q-page padding>
     <AddLink />
+
+    <template v-for="link in urlStore.links" :key="link._id">
+      <LinkCard />
+    </template>
 
     <pre>{{ urlStore.links }}</pre>
   </q-page>
