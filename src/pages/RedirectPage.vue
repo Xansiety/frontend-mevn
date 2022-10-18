bd753eabe59e5a338a9477eeb4614a1ad7909817
xansiety <ferando543@outlook.com>
2022-10-17 19:14:35 -0500

    Redireccionamiento desde nanoLink al copiar

---
+++ src/pages/RedirectPage.vue
@@ -0,0 +1,8 @@
+<template>
+  <q-page padding class="text-center q-pt-xl">
+    <h3>Te estamos redirigiendo...</h3>
+    <q-spinner color="primary" size="3em" :thickness="10" />
+  </q-page>
+</template>
+
+<style lang="scss" scoped></style>
